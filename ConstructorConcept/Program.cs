debe1c9dccbcc2b8b5c1919dd23f1a865d9e07eb
Pratul Makar <pratulmakar02@gmail.com>
2024-08-14 16:37:40 +0530

    new

---
+++ ConstructorConcept/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Anime obj1 = new Anime();
+            Anime obj2 = new Anime("Naruto","Adventure",9.5);
+            Anime obj3 = new Anime(obj2);
+        }
+    }
+
+    public class Anime
+    {
+        public string animeName;
+        public string generName;
+        public double imDbRating;
+
+        public Anime()
+        {
+            Console.WriteLine("This is default Constructor.");
+        }
+
+        public Anime(string animeName, string generName, double imDbRating)
+        {
+            Console.WriteLine("Parameterized Constructor.");
+            Console.WriteLine("Name :" +animeName);
+            Console.WriteLine("Gener :" + generName);
+            Console.WriteLine("Rating :" + imDbRating);
+        }
+
+        public Anime(Anime anime)
+        {
+            Console.WriteLine("Copy Constructor");
+            animeName = anime.animeName;
+            generName = anime.generName;
+            imDbRating = anime.imDbRating;
+        }
+
+
+    }
+}
--- OOPS/Program.cs
+++ OOPS/Program.cs
@@ -10,7 +10,41 @@ namespace OOPS
     {
         static void Main(string[] args)
         {
+//Reaference variable of Class Student
+            Student obj1 = new Student(); //Creating object of class Student
+            obj1.name = "Pratul";
+            obj1.rollNo = 1;
+            obj1.email = "pratulmakar02@gmail.com";
+            obj1.Display();
+            //static method can be called without creating object reference
+            Console.WriteLine(Student.age(20));
+            Student.method1();
+        }
+    }
 
+    class Student
+    {
+        public static int age(int age)
+        {
+            return age;
+        }
+        //instance variables
+        public string name;
+        public int rollNo;
+        public string email;
+
+        public void Display()// public method/function display
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Roll No: " + rollNo);
+            Console.WriteLine("Email: " + email);
+        }
+        // We can not use non-static variable in static method without creating object reference
+        public static void method1()
+        {
+            Student obj = new Student();
+            obj.name = "Pratul";
+            Console.WriteLine(obj.name);
         }
     }
 }
