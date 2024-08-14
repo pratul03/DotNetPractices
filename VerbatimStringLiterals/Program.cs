973d719992a90b6c359242d9180e8f50d56ff5b0
Pratul Makar <pratulmakar02@gmail.com>
2024-08-14 21:12:34 +0530

    updated

---
+++ VerbatimStringLiterals/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerbatimStringLiterals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Desktop\";
+            Console.WriteLine(path);
+            string speech = @"Hello ""I am Pratul Makar. "" ";
+            Console.WriteLine(speech);
+        }
+    }
+}
